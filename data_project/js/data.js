80a81fda772c3385f7259182c4a091c353a1b63d
aram <dev.aram0717@gmail.com>
2023-11-10 16:27:33 +0900

    공공기관 데이터 과제 작업 완료

--- data_project/js/data.js
+++ data_project/js/data.js
@@ -41,8 +41,12 @@ async function fetchData(){
             btnValue = $(this).text()
             dataSet(btnValue)
 
+            $('.info').hide();
             $('.sec3').fadeIn();
+            $('.sec3 .right').hide();
         })
+
+
         // 천 단위로 , 찍기
         allTotal = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
@@ -50,19 +54,35 @@ async function fetchData(){
         document.querySelector('.dataTot').innerText = `/ ${allTotal}건`
 
 
-        // 대분류 별 토탈값
-        function dataSet(dataValue = '강력범죄') {  
+        // 대분류 및 전체 토탈값
+        function dataSet(dataValue = '강력범죄',) {  
             let Obj = []; // 대분류, 중분류 제외 배열   
+            let subCate = []; // 중분류 리스트
+            let key = ''; //키값
+            let value = '';//벨류값
+            let txt = '';
             cateTotal = 0 
             for(value in data){
                 if(data[value]['범죄대분류'] == dataValue){
                     const newObj = { ...data[value] };
                     delete newObj['범죄대분류'];
                     delete newObj['범죄중분류'];
-                    Obj.push(newObj)  
+                    subCate.push(data[value]['범죄중분류']);
+                    Obj.push(newObj)
+                    
                 }
             }
-    
+
+            // 서브카테 리스트 출력
+            const subCateUl = document.querySelector('.sub-cate');
+            subCate.forEach((el, idx)=>{
+                txt +=
+                `
+                <li><button type="button" class="type-2">${el}</button></li>
+                `
+            })
+            subCateUl.innerHTML = txt
+        
             // 대분류 범죄건수 total값 구하기
             Obj.forEach((el, idx)=>{
                 for(let i=0;i<Object.keys(el).length;i++){
@@ -82,7 +102,45 @@ async function fetchData(){
             // dataTot 에 값 출력
             document.querySelector('.cateTot').innerText = `${cateTotal}건`
 
-        }    
+        }
+        // 범죄 소카테고리
+        $(document).on('click','.sub-cate button',function () {
+            $(this).addClass('on')
+            $('.sub-cate button').not(this).removeClass('on')
+            btnValue = $(this).text();
+            tableDataSet(btnValue)
+            $('.sec3 .right').fadeIn();
+        })
+        
+        function tableDataSet(btnValue) {
+            let Obj = []; // 대분류, 중분류 제외 배열   
+            for(value in data){
+                if(data[value]['범죄중분류'] == btnValue){
+                    const newObj = { ...data[value] };
+                    delete newObj['범죄대분류'];
+                    delete newObj['범죄중분류'];
+                    Obj.push(newObj)
+                    
+                }
+            }
+
+            const table = document.querySelector('table'); //테이블
+            let dataKey = Object.keys(Obj[0]); //키값
+            let dataValue = Object.values(Obj[0]);//벨류값
+            let txt = '';
+
+            // 테이블 생성
+            dataKey.forEach((e,idx)=>{
+                txt +=
+                `
+                <tr>
+                    <th>${dataKey[idx]}</th>
+                    <td>${dataValue[idx]}건</td>
+                </tr>
+                `
+            })
+            table.innerHTML = txt
+        }
     } catch (error) {
         console.error('API 요청 오류:', error);
     }
