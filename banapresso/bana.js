b6b559f17068d15b0b2f2780ce5f4b41966ee3c8
dev.aram <dev.aram0717@gmail.com>
2023-11-28 12:53:03 +0900

    바나프레소 완료

--- banapresso/bana.js
+++ banapresso/bana.js
@@ -17,46 +17,38 @@ fetch('http://localhost:8080/bana/')
         `
     });
     ul.innerHTML = txt
-    map(data)
-})
+    
 
-
-
-function map(data) {
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };  
+        center: new kakao.maps.LatLng(36.91671, 127.7226), // 지도의 중심좌표
+        level: 12 // 지도의 확대 레벨
+    };
 
     // 지도를 생성합니다    
-    var map = new kakao.maps.Map(mapContainer, mapOption); 
+    var map = new kakao.maps.Map(mapContainer, mapOption);
 
-    // 주소-좌표 변환 객체를 생성합니다
-    var geocoder = new kakao.maps.services.Geocoder();
+    data.forEach(element => {
+        var geocoder = new kakao.maps.services.Geocoder();
+        geocoder.addressSearch(element.addr, function (result, status) {
 
-    // 주소로 좌표를 검색합니다
-    geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function(result, status) {
+            // 정상적으로 검색이 완료됐으면 
+            if (status === kakao.maps.services.Status.OK) {
 
-        // 정상적으로 검색이 완료됐으면 
-        if (status === kakao.maps.services.Status.OK) {
+                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+                // 결과값으로 받은 위치를 마커로 표시합니다
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: coords
+                });
 
-            // 결과값으로 받은 위치를 마커로 표시합니다
-            var marker = new kakao.maps.Marker({
-                map: map,
-                position: coords
-            });
+                // 인포윈도우로 장소에 대한 설명을 표시합니다
+                var infowindow = new kakao.maps.InfoWindow({
+                    content: element.addr
+                });
+            }
+        });
+    })
 
-            // 인포윈도우로 장소에 대한 설명을 표시합니다
-            var infowindow = new kakao.maps.InfoWindow({
-                content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
-            });
-            infowindow.open(map, marker);
-
-            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-            map.setCenter(coords);
-        } 
-    });    
-}
+})
